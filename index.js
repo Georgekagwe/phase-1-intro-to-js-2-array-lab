6043d05aa65803c17a57a3585f3cb06be205a3c0
George <george.kagwe@student.moringaschool.com>
2025-03-12 07:27:42 +0300

    array lab intro to js 2 completed

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.splice(2,1)
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.splice(0,1) 
+}
+
+
+function appendCat(cats, cat) {
+  return [...cats, cat]; 
+}
+
+function appendCat(cats, name) {
+    return [...cats, name];
+  }
+  
+ 
+  function appendCat (name) {
+    return [...cats, name];
+  }
+  
+  function prependCat (name) {
+    return [name, ...cats];
+  }
+  
+  function removeFirstCat () {
+    return cats.slice(1);
+  }
+  
+  function removeLastCat () {
+    return cats.slice(0, cats.length - 1);
+  }
+
+
+
+
+
+
+  
+  
